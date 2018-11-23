06fc4767ecec083f9d848ce1afad881350dbc96e
Hobin <name.milton@gmail.com>
2018-11-23 11:27:05 +0530

    accordion trials
    Signed-off-by: Hobin <name.milton@gmail.com>

---
+++ accordian-filter/script.js
@@ -0,0 +1,65 @@
+function onload() {
+    accordionAlgo();
+    accordionDisplayMode();
+    accordionCreationDate();
+}
+
+function accordionAlgo() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", toggleItem, false);
+    }
+
+    function toggleItem() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
+}
+
+function accordionDisplayMode() {
+    var dmItem = document.getElementsByClassName('dm-flex-item');
+    for (i = 0; i < dmItem.length; i++) {
+        dmItem[i].addEventListener('click', toggleDmItem, false);
+    }
+
+    function toggleDmItem() {
+        if (this.className != 'dm-flex-item modeOn') {
+            for (i = 0; i < this.parentNode.children.length; i++) {
+                console.log("clazz " + dmItem[i].className);
+                if (dmItem[i].className == 'dm-flex-item modeOff') {
+                    dmItem[i].className = 'dm-flex-item modeOn';
+                } else {
+                    dmItem[i].className = 'dm-flex-item modeOff';
+                }
+            }
+
+        }
+    }
+}
+function accordionCreationDate() {
+    let dmItem = document.getElementsByClassName('cd-flex-item');
+    for (i = 0; i < dmItem.length; i++) {
+        dmItem[i].addEventListener('click', toggleDmItem, false);
+    }
+
+    function toggleDmItem() {
+        if (this.className != 'cd-flex-item modeOn') {
+            for (i = 0; i < this.parentNode.children.length; i++) {
+                console.log("clazz " + dmItem[i].className);
+                if (dmItem[i].className == 'cd-flex-item modeOff') {
+                    dmItem[i].className = 'cd-flex-item modeOn';
+                } else {
+                    dmItem[i].className = 'cd-flex-item modeOff';
+                }
+            }
+
+        }
+    }
+}
